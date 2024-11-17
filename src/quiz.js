95fcacdb8763e35ca5ff5e414ffb6ab953741314
Marian Bak <mlazik1@gmail.com>
2024-11-17 16:51:47 +0100

    implemented everything up until day 3

--- src/quiz.js
+++ src/quiz.js
@@ -1,69 +1,74 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+  // YOUR CODE HERE:
+  //
+  // 1. constructor (questions, timeLimit, timeRemaining)
 
-    constructor (questions, timeLimit, timeRemaining){
-        this.questions = questions;
-        this.timeLimit = timeLimit;
-        this.timeRemaining = timeRemaining;
-        this.correctAnswers = 0;
-        this.currentQuestionIndex = 0;
-    }
-   
-    // 2. getQuestion()
-    getQuestion(){
-        return this.questions[this.currentQuestionIndex];
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
 
-    }
-    
-    // 3. moveToNextQuestion()
+  // 2. getQuestion()
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
 
-    moveToNextQuestion(){
-        this.currentQuestionIndex += 1;
-    }
+  // 3. moveToNextQuestion()
 
-    // 4. shuffleQuestions()
+  moveToNextQuestion() {
+    this.currentQuestionIndex += 1;
+  }
 
-    shuffleQuestions(){
-    for (let i = this.questions.length - 1; i > 0; i--){
-        const randomIndex = Math.floor(Math.random() * (i + 1));
-        [this.questions[i], this.questions[randomIndex]] = [this.questions[randomIndex], this.questions[i]];
+  // 4. shuffleQuestions()
+
+  shuffleQuestions() {
+    for (let i = this.questions.length - 1; i > 0; i--) {
+      const randomIndex = Math.floor(Math.random() * (i + 1));
+      [this.questions[i], this.questions[randomIndex]] = [
+        this.questions[randomIndex],
+        this.questions[i],
+      ];
     }
     this.currentQuestionIndex = 0;
-}
+  }
 
-    // 5. checkAnswer(answer)
+  // 5. checkAnswer(answer)
 
-    checkAnswer(answer){
-        
-        const currentQuestion = this.questions[this.currentQuestionIndex];
-       
-        if (answer === currentQuestion.answer) {
-            this.correctAnswers += 1;
-            return true;
-        }
-        return false;
+  checkAnswer(answer) {
+    const currentQuestion = this.questions[this.currentQuestionIndex];
+
+    if (answer === currentQuestion.answer) {
+      this.correctAnswers += 1;
+      return true;
     }
+    return false;
+  }
 
-    // 6. hasEnded()
+  // 6. hasEnded()
 
-    hasEnded(){
+  hasEnded() {
     return this.currentQuestionIndex >= this.questions.length;
-}
+  }
 
+  filterQuestionsByDifficulty(difficulty) {
+    if (difficulty < 1 || difficulty > 3) return;
 
-   filterQuestionsByDifficulty(difficulty){
-        this.difficulty = difficulty;
-           
-   
-    }
+    this.questions = this.questions.filter(
+      (question) => question.difficulty <= difficulty
+    );
+  }
 
+  averageDifficulty() {
+    let sumDifficulty = this.questions.reduce((counter, question) => {
+      return counter + question.difficulty;
+    }, 0);
+    return sumDifficulty / this.questions.length;
+  }
 
-    averageDifficulty(){
-        return 
-
-
-    }
-
+  getProgress() {
+    return (this.currentQuestionIndex / this.questions.length) * 100;
+  }
 }
